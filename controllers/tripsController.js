0d611376693d2c5093819edaa7a1320e644c47b6
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-06-19 20:55:04 +1000

    start 2

---
+++ controllers/tripsController.js
@@ -0,0 +1,9 @@
+// add trip
+// delete trip
+// edit trip (trip's detail, name description)
+// link the trip to a particular DB user
+// get trip
+
+// add destination to trip
+// edit dstination information
+// delete destination
