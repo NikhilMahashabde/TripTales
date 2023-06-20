b9a632329dc3abf678bcfb7b88fda25f872e6e39
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-06-20 10:52:20 +1000

    clean up

--- server.js
+++ server.js
@@ -1,43 +1,49 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const port = process.env.PORT || 3001;
+const path = require("path");
+const mongoose = require("mongoose");
+const connectDB = require("./config/mongoConfig");
+const PORT = process.env.PORT || 3001;
+// const enableSession = require("./middleware/session");
 
-const { MongoClient } = require("mongodb");
-const mongoClient = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING);
-let testCollection;
+//middleware
 
-mongoClient
-  .connect()
-  .then((_) => {
-    const db = mongoClient.db("test");
-    testCollection = db.collection("test");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// app.use(enableSession);
+connectDB();
 
-app.get("/", (_, response) => {
-  response.json({ server: "is alive" });
-});
+// mongoClient
+//   .connect()
+//   .then((_) => {
+//     const db = mongoClient.db("test");
+//     testCollection = db.collection("test");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
-app.get("/api/test", (_, response) => {
-  testCollection
-    .find()
-    .toArray()
-    .then((result) => {
-      response.json(result);
-    });
-});
+// app.get("/api/test", (_, response) => {
+//   testCollection
+//     .find()
+//     .toArray()
+//     .then((result) => {
+//       response.json(result);
+//     });
+// });
 
-app.post("/api/test", (request, response) => {
-  testCollection.insertOne(request.body).then((_) => {
-    response.json();
-  });
-});
+// app.post("/api/test", (request, response) => {
+//   testCollection.insertOne(request.body).then((_) => {
+//     response.json();
+//   });
+// });
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+app.use("/", express.static(path.join(__dirname, "client"))); // static file resources
+app.use("/js", express.static(path.join(__dirname, "views", "js")));
+
+//After mongoose client opens a successfull connection to the database, check if it returns "open", and if so, run the callback below which turns on the server.
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 });
