9c022df4715105393b30c8624b9f9ff975e902f3
MKuma5555 <126067609+MKuma5555@users.noreply.github.com>
2023-06-24 21:54:34 +1000

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { getAllUsers } from "../../controllers/usersController.js";
-// import { verifyLoggedIn } from "../../controllers/sessionController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllUsers);
 router.get("/", getAllUsers);
-// router.get("/", verifyLoggedIn);
 
 
 export default router;
